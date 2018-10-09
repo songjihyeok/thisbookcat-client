4c249acc810cde9bbedaa7f9819ec3eb9c9a771e
Roh Won Seok <wonseok96@gmail.com>
2018-10-09 17:47:54 +0900

    this is first

---
+++ src/components/Nav1.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  FormControl,
+  FormGroup,
+  Button
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Icon } from "semantic-ui-react";
+import "./Nav1.css";
+
+class Nav1 extends Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/main">
+              <div className="thisBook">이 책 반 냥</div>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <FormControl type="text" placeholder="Search Books" />
+            </FormGroup>
+            <Button type="submit">
+              <Icon name="search" size="large" />
+            </Button>
+          </Navbar.Form>
+          <Nav pullRight>
+            <NavItem eventKey={2} href="/main">
+              <Icon name="tags" size="big" />
+            </NavItem>
+            <NavItem eventKey={2} href="/mylike">
+              <Icon name="heart outline" size="big" />
+            </NavItem>
+            <NavItem eventKey={2} href="/writepost">
+              <Icon name="pencil" size="big" />
+            </NavItem>
+            <NavItem eventKey={2} href="/mypage">
+              <Icon name="user circle" size="big" />
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Nav1;
---
+++ src/routes/MyPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Nav1 from "../components/Nav1";
+// import MyBooks from "../components/MyPage/MyBooks";
+// import Profile from "../components/MyPage/Profile";
+
+class MyPage extends Component {
+  render() {
+    return (
+      <div>
+        <Nav1 />
+      </div>
+    );
+  }
+}
+
+export default MyPage;
