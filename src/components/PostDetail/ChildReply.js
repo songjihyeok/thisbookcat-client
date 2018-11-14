61509d2a49b1d5b237f32e8b56c3342afc08d7a3
SUEMIN JEONG <sueminee@SUEMINui-MacBook-Air.local>
2018-11-15 02:59:38 +0900

    stupid mistake

--- src/components/PostDetail/ChildReply.js
+++ src/components/PostDetail/ChildReply.js
@@ -8,13 +8,16 @@ export default class ChildReply extends Component {
     show_reReplyInput: false,
   }
 
+  reComment = {};
+  authHeader = {headers:{Authorization: `bearer ${window.localStorage.getItem('token')}`}};
+
   _handleReReplyInput = () => {
     let show_reReplyInput = this.state.show_reReplyInput
     this.setState({show_reReplyInput : !show_reReplyInput})
   }
 
   _newReReply = (e) => {
-    console.log('ParentReply.js 컴포넌트의 _newReReply함수에서 e.target.value', e.target.value)
+    // console.log('ParentReply.js 컴포넌트의 _newReReply함수에서 e.target.value', e.target.value)
     const { userId, id, } = this.props.reply
     this.reComment = {
       replyContents: `@${userId} ${e.target.value}`,
@@ -23,11 +26,12 @@ export default class ChildReply extends Component {
   }
 
   _makeReReply = async() => {
-    console.log('ParentReply.js 컴포넌트의 _makeReReply 함수에서 this.reComment', this.reComment);
-    const res_postReReply = await axios.post(`http://${server_url}:3000/api/reply2/${this.props.postId}`
+    // console.log('ParentReply.js 컴포넌트의 _makeReReply 함수에서 this.reComment', this.reComment);
+    // const res_postReReply = 
+    await axios.post(`http://${server_url}:3000/api/reply2/${this.props.postId}`
       , this.reComment
       , this.authHeader)
-    console.log('Reply.js 컴포 > _makeReReply 함수 > axios.post 요청 후 받는 res_postReReply', res_postReReply);
+    // console.log('Reply.js 컴포 > _makeReReply 함수 > axios.post 요청 후 받는 res_postReReply', res_postReReply);
     await this.props._getReply();
   }
 
@@ -43,6 +47,7 @@ export default class ChildReply extends Component {
         <span onClick={this._handleReReplyInput}>답글</span>
         
         {/* 이 아래는 대댓글을 쓰고싶다고 하면! 인풋창을 보여주고, 아니면 나타나지 않는 부분입니다. */}
+        {/* TODO:여기도 컴포넌트화 해야하나 */}
         <div>
              {(this.state.show_reReplyInput) 
              ?
@@ -67,59 +72,3 @@ export default class ChildReply extends Component {
     )
   }
 }
-
-
-/* <div className='reply'>
-          <span className='reply_username'>{parentReply.userId} </span>
-          <span className='reply_msg'>{parentReply.replyContents} </span>
-          <span onClick={this._showRereplyInput}>
-            <Icon name="pencil alternate" color='grey' fitted size="small" />
-          </span>
-          <div>
-            {(this.state.re_reply_input)
-            ?
-              <div id="rereply">
-                <form>
-                <input
-                  id="rereply_input"
-                  type="text"
-                  name="reply"
-                  placeholder={`@${parentReply.userId}`}
-                  onChange={this._newReReply}
-                />
-                <div id="rereply_btn" onClick={this._makeReReply}>
-                  <Icon name="pencil alternate" bordered inverted color='grey' fitted size="small" />
-                </div>
-                </form>
-              </div>
-            : <div></div>
-            }
-            </div>
-          </div>
-        :
-          <div className='rereply'>
-            <span className='reply_username'>{parentReply.userId} </span>
-            <span className='reply_msg'>{parentReply.replyContents} </span>
-            <span onClick={this._showRereplyInput}>
-              <Icon name="pencil alternate" color='grey' fitted size="small" />
-            </span>
-            <div>
-              {(this.state.re_reply_input)
-              ?
-                <div id="rereply">
-                  <form>
-                  <input
-                    id="rereply_input"
-                    type="text"
-                    name="reply"
-                    placeholder={`@${parentReply.userId}`}
-                    onChange={this._newReReply}
-                  />
-                  <div id="rereply_btn" onClick={this._makeReReply}>
-                    <Icon name="pencil alternate" bordered inverted color='grey' fitted size="small" />
-                  </div>
-                  </form>
-                </div>
-              : <div></div>
-              }
-            </div>  */
