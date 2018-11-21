010303affe79fce3265d72cbdf4be393a1aa151b
SONGJIHYEOK <porr3444@naver.com>
2018-11-21 12:36:25 +0900

    searchPage

--- src/routes/TagSearchPage.js
+++ src/routes/TagSearchPage.js
@@ -5,21 +5,42 @@ import server_url from '../url.json';
 import Nav1 from "../components/Nav1";
 import BookBoard from "../components/Main/BookBoard";
 
+
 class TagSearchPage extends Component {
   
-  
-  state = {
-    per: 8,
-    //한페이지당 가지게될 포스트의 개수
-    page: 1,
-    //정해진 per만큼의 포스트를 가지는 페이지
-    totalPage:''
-  };
+  constructor (props){
+    super(props)
+    this.state = {
+      per: 8,
+      //한페이지당 가지게될 포스트의 개수
+      page: 1,
+      //정해진 per만큼의 포스트를 가지는 페이지
+      totalPage:'',
+      coverurl: null,
+      tagName : this.props.match.params.TagName
+    }
+    console.log("construtor")
+  }
 
-  componentDidMount() {
-    this._getUrls()
-    window.addEventListener('scroll', this._infiniteScroll, true)
-   // this._getMyProfile()
+  shouldComponentUpdate(nextProps,nextState){
+    const isthetagNamechanged = nextState.coverurl!== null
+    return isthetagNamechanged
+  }
+
+  componentDidMount () {
+      this._getUrls().then(()=>{
+        window.addEventListener('scroll', this._infiniteScroll, true)
+       })
+  }
+
+  async componentWillReceiveProps (nextProps) {
+    console.log(this.props.match.params.TagName, "and", nextProps.match.params.TagName)
+    if (this.props.match.params.TagName !== nextProps.match.params.TagName) {
+      console.log("다르네!")
+      await this.setState(()=>({coverurl: null}))
+      await this.setState({tagName: nextProps.match.params.TagName})
+      await this._getUrls();
+    }
   }
 
   _infiniteScroll = () => {
@@ -33,9 +54,9 @@ class TagSearchPage extends Component {
     if(scrollTop + clientHeight === scrollHeight) {
 
       if(this.state.page!==this.state.totalPage) {
-        this.setState({
-          page: this.state.page+1
-        })
+        this.setState((state)=>({
+          page: state.page+1
+        }))
         this._getUrls()
       }
     }
@@ -60,30 +81,30 @@ class TagSearchPage extends Component {
 
     console.log("get url 과정?",coverurl)
     
-    if(this.state.coverurl===undefined) {
-      this.setState({
+    if(this.state.coverurl===null) {
+      await this.setState({
         coverurl
       })
     } else {
-      this.setState({
+      await this.setState({
         coverurl: this.state.coverurl.concat(coverurl)
       })
     }
   };
 
-  _callTheTagBookCoverAPI = () => {
+  _callTheTagBookCoverAPI = async() => {
     console.log("uri 가져옵시다")
     let token = window.localStorage.getItem('token')
 
-    return axios.post(`http://${server_url}:3000/api/post/tag/${this.state.per}/${this.state.page}`,{tagName : this.props.match.params.TagName},{headers:{Authorization: `bearer ${token}`}})
-    .then((response) => {
+    return  axios.post(`http://${server_url}:3000/api/post/tag/${this.state.per}/${this.state.page}`,{tagName : this.state.tagName},{headers:{Authorization: `bearer ${token}`}})
+    .then(async(response) => {
       console.log('there should be data here',response.data)
-      this.setState({
+      await this.setState({
         totalPage: response.data.totalpage
       })
-      let result = response.data.perArray
-      console.log(result)
-      return result;
+        let result = response.data.perArray
+        console.log("결과물은??_______",result)
+        return result;
       })
       .catch(err => console.log(err))
   };
@@ -95,12 +116,11 @@ class TagSearchPage extends Component {
     console.log(window.localStorage.getItem('token'))
     console.log('this is totalpage', this.state.totalPage)
     console.log("이미 커버유알엘?",this.state.coverurl)
-    
     //토큰이 없으면 로그인 페이지로 가라.
     if(!window.localStorage.getItem("token")){
       console.log("picka")
       return <Redirect to="/login" />
-    }else{
+    } else {
     return (
       <div className="Main">
         <Nav1/>
