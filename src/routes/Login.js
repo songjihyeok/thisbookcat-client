6f3b6f972b848c4748b93f7383c916d9e5ac6d1d
SUEMIN JEONG <sueminee@SUEMINui-MacBook-Air.local>
2018-11-23 16:18:41 +0900

    login css alomost done

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -3,10 +3,12 @@ import React, { Component } from "react";
 import '../components/Login/Login.css';
 // import { Icon } from "semantic-ui-react";
 import { Link, Redirect } from "react-router-dom";
-import { Button } from 'react-bootstrap'
+// import { Button } from 'react-bootstrap'
 import axios from 'axios';
 import server_url from '../url.json';
-// import mainImg from '../img/main-logo.svg';
+import book from "../img/book-img.png";
+// import book2 from "../img/book-img@2x.png";
+// import book3 from "../img/book-img@3x.png";
 
 class Login extends Component {
   state = {
@@ -53,13 +55,6 @@ class Login extends Component {
               isLogin : false,
               login_err : true})
     }
-    
-      // .catch(err => {
-      //   console.log('login.js > _handleSubmit함수에서 axios.post 요청보냈는데, err___', err)
-      //   this.setState({
-      //         isLogin : false,
-      //         login_err : true})
-      // })
   }
 
   // _googleAuth = (e) => {
@@ -80,52 +75,61 @@ class Login extends Component {
     } else {
       return (
         <div className="backImg">
-          <div className='login_container' >
-            <div className='login_container_1'>
-              <div className='login_container_2'>
-              <div className='title1'>북컨텐츠를 모두와 함께 공유하는</div>
-              <div className='title2'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="166" height="44">
-                  <text fill="#FEFEFE" fontFamily="BM DoHyeon OTF" fontSize="47.061" transform="translate(.392 35.64) scale(.93495)">
-                    이책반냥
-                  </text>
-                </svg>
-              </div>
-              
+          {/* <div className='login_container' >
+            <div className='login_container_1'> */}
+              {/* <div className='login_body'> */}
+                <div className='login_body'>
+                  <div className='login_container'>
+                    <div className='title1'>북컨텐츠를 모두와 함께 공유하는</div>
+                    <div className='title2'>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="166" height="44">
+                        <text fill="#FEFEFE" fontFamily="BM DoHyeon OTF" fontSize="47.061" transform="translate(.392 35.64) scale(.93495)">
+                          이책반냥
+                        </text>
+                      </svg>
+                    </div>
+                    
 
-              {/* <div className='title3'>이책반냥에 오신 것을 환영합니다</div> */}
-              {(this.state.login_err)
-              ?
-                <div className='title4'>이메일 혹은 비밀번호가 올바르지 않습니다</div>
- 
-              : <div className='title3'>이책반냥에 오신 것을 환영합니다</div>
-              }
-              </div>
-              <form onSubmit={this._handleSubmit}>
-                <div><input className='login_input' type="email" placeholder="이메일을 입력해주세요"
-                            onChange={this._setEmail}/>
+                    {/* <div className='title3'>이책반냥에 오신 것을 환영합니다</div> */}
+                    {(this.state.login_err)
+                    ?
+                      <div className='title4'>이메일 혹은 비밀번호가 올바르지 않습니다</div>
+      
+                    : <div className='title3'>이책반냥에 오신 것을 환영합니다</div>
+                    }
+                    
+                    <form onSubmit={this._handleSubmit}>
+                      <div><input className='login_input' type="email" placeholder="이메일을 입력해주세요"
+                                  onChange={this._setEmail}/>
+                      </div>
+                      <div><input className='login_input' type="password" placeholder="비밀번호를 입력해주세요"
+                                  onChange={this._setPassword}/>
+                      </div>
+                      <div><button id="custom_btn_continue" type='submit' className='login_btn'>계속하기</button></div>
+                    </form>
+                    <div style={{color: '#ffffff', fontSize: '13.8px', marginTop: '14px', marginBottom: '24px'}}>
+                      또는
+                    </div>
+                    <div><button id="custom_btn_facebook" className='login_btn'>FACEBOOK으로 계속하기</button></div>
+                    <div><button id="custom_btn_google" className='login_btn' onClick={this._googleAuth}>GOOGLE로 계속하기</button></div>
+                    {/* <div><button className='login_btn'>FACEBOOK으로 계속하기</button></div>
+                    <div><Button className='login_btn' onClick={this._googleAuth}>GOOGLE로 계속하기</Button></div> */}
+                    {/* <div className='login_privacy'>{`계속하면 이책반냥 서비스 약관 및 개인정보 보호 정책에 동의하는 것으로 간주합니다.`}</div> */}
+                    {/* TODO: 재플린에는 위의 내용이 없습니다용? */}
+                    <div className='login_flex'>
+                      <Link to="/signup"><div style={{color: 'rgba(255, 255, 255, 0.5)'}}>회원가입</div></Link>
+                      <Link to="/findpw"><div style={{color: 'rgba(255, 255, 255, 0.5)'}}>아이디/비밀번호 찾기</div></Link>
+                    </div>
+                  </div>
                 </div>
-                <div><input className='login_input' type="password" placeholder="비밀번호를 입력해주세요"
-                            onChange={this._setPassword}/>
+              <div className="footer">
+                <div>
+                  <img className="book_deco" src={book} style={{width: '85%', height: 'auto'}} alt='deco'></img>
                 </div>
-                <div><Button bsClass="custom_btn_continue" type='submit' className='login_btn'>계속하기</Button></div>
-              </form>
-              <div style={{color: '#ffffff', fontSize: '13.8px', marginTop: '14px', marginBottom: '24px'}}>
-                또는
-              </div>
-              <div><Button bsClass="custom_btn_facebook" className='login_btn'>FACEBOOK으로 계속하기</Button></div>
-              <div><Button bsClass="custom_btn_google" className='login_btn' onClick={this._googleAuth}>GOOGLE로 계속하기</Button></div>
-              {/* <div><button className='login_btn'>FACEBOOK으로 계속하기</button></div>
-              <div><Button className='login_btn' onClick={this._googleAuth}>GOOGLE로 계속하기</Button></div> */}
-              {/* <div className='login_privacy'>{`계속하면 이책반냥 서비스 약관 및 개인정보 보호 정책에 동의하는 것으로 간주합니다.`}</div> */}
-              {/* TODO: 재플린에는 위의 내용이 없습니다용? */}
-              <div className='login_flex'>
-                <Link to="/signup"><div style={{color: 'rgba(255, 255, 255, 0.5)'}}>회원가입</div></Link>
-                <Link to="/findpw"><div style={{color: 'rgba(255, 255, 255, 0.5)'}}>아이디/비밀번호 찾기</div></Link>
               </div>
             </div>
-          </div>
-        </div>
+          /* </div>
+        </div> */
     );
   }}
 }
