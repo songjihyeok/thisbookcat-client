0ebbca22f2d0882097bd40a094f056bacc281c1d
unknown <grayrays@gmail.com>
2019-02-27 12:12:37 +0900

    policy modal mark up

---
+++ src/routes/ModalAgree.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import {Modal, Button} from 'react-bootstrap'
+import style from '../components/MyPage/SettingModal.css'
+import Using from './PolicyHtml/usingPolicy.js'
+import Privacy from './PolicyHtml/privacyPolicy.js'
+
+class ModalAgree extends Component {
+  render() {
+    return (
+      <Modal show={this.props.show} container={this}
+            onHide={this.props.hide} aria-labelledby="contained-modal-title">
+        <Modal.Body>
+        <div className="agreeLayout">
+          <div className="agreeContent" id="usingPolicy">
+            <h3>이용약관</h3>
+            <div className="policy"><Using /></div>
+            <p className="agreeCheck"><input type="checkbox" id="agreeUsing" /><label for="agreeUsing">동의합니다</label></p>
+          </div>
+
+          <div className="agreeContent" id="privacyPolicy">
+            <h3>개인정보 보호 약관</h3>
+            <div className="policy"><Privacy /></div>
+            <p className="agreeCheck"><input type="checkbox" id="privacyUsing" /><label for="privacyUsing">동의합니다</label></p>
+          </div>
+        </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this._handleConfirm}>동의</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}  
+  
+export default ModalAgree
