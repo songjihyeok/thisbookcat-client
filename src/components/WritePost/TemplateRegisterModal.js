88d4d7c3e732532d8182cad22ffd1c540a765531
hanyoungjae <youngjae.han@tapjoy.com>
2019-03-17 03:07:27 +0900

    test textarea

--- src/components/WritePost/TemplateRegisterModal.js
+++ src/components/WritePost/TemplateRegisterModal.js
@@ -15,8 +15,8 @@ class TemplateRegisterModal extends Component {
       onUpload,
       onClick
     } = this.props;
-    
     const backgroundImage = !selectedImg ? null : FAKEDATA.find((e) => selectedImg === e.id).img;
+
     return (
       <Modal show container={this} aria-labelledby="contained-modal-title">
         <Modal.Header className="template-register-modal-header-container">
@@ -33,6 +33,7 @@ class TemplateRegisterModal extends Component {
                   className="template-register-modal-textarea" 
                   value={text}
                   onChange={onChange}
+                  onKeyDown={this.handleKeyDown}
                 />
               </>
                 :
@@ -45,7 +46,7 @@ class TemplateRegisterModal extends Component {
         <Modal.Footer>
           <Button 
             bsStyle="success" 
-            disabled={!selectedImg}
+            disabled={!(selectedImg && text)}
             onClick={onUpload}
           >
             등록
@@ -58,6 +59,24 @@ class TemplateRegisterModal extends Component {
   handleSelectImage(id) {
     const { onSelect } = this.props;
     onSelect(id);
+  }
+
+  handleKeyDown(e) {
+    // Reset field height
+    e.target.style.height = 'inherit';
+
+    // Get the computed styles for the element
+    const computed = window.getComputedStyle(e.target);
+
+    // Calculate the height
+    const height = parseInt(computed.getPropertyValue('border-top-width'), 10) +
+      parseInt(computed.getPropertyValue('padding-top'), 10) +
+      e.target.scrollHeight +
+      parseInt(computed.getPropertyValue('padding-bottom'), 10) +
+      parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+
+    e.target.style.height = `${height}px`;
+    // e.target.style.paddingTop = `${Math.max(0, (height * 0.5)) - 100}px`;
   }
 }
 
