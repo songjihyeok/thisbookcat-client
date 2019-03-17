4e416be7ca95b3eacf0700e01d5ec4e159f45903
hanyoungjae <youngjae.han@tapjoy.com>
2019-03-17 17:31:47 +0900

    apply template icon

--- src/components/WritePost/TemplateRegisterModal.js
+++ src/components/WritePost/TemplateRegisterModal.js
@@ -21,7 +21,10 @@ class TemplateRegisterModal extends Component {
       <Modal show container={this} aria-labelledby="contained-modal-title">
         <Modal.Header className="template-register-modal-header-container">
           <Modal.Title id="contained-modal-title" className="template-register-modal-header-title">사진 등록</Modal.Title>
-          <div className="template-register-modal-header-icon" onClick={onClick}>템플릿</div>
+          <div  className="template-register-modal-header-left" onClick={onClick}>
+            <div className="template-register-modal-header-left-icon" />
+            <div className="template-register-modal-header-left-text">템플릿</div>
+          </div>
         </Modal.Header>
         <Modal.Body>
           <div className="template-register-modal-body-container">
